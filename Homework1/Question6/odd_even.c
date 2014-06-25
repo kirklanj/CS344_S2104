e4e1261fd2dddd4f41157594e9fe13b309b2a522
kirklanj <kirklanj@onid.oregonstate.edu>
2014-06-24 22:47:00 -0700

    testing commit

---
+++ Homework1/Question6/odd_even.c
@@ -0,0 +1,31 @@
+/*
+
+  $RCSfile$
+  $Revision$
+  $Author$
+  $Date$
+  $Log$
+
+  from:
+  http://www.programmingsimplified.com/c/source-code/c-program-check-odd-even
+
+ */
+
+
+#include<stdio.h>
+ 
+main()
+{
+   int n;
+ 
+   printf("Enter an integer\n");
+   scanf("%d", &n);
+ 
+   if ( n & 1 == 1 ))
+   {
+      printf("Odd\n");
+   else
+      printf("Even\n");
+ 
+   return 0;
+}
